c125bc656a59ec3f347ee10aae746a59d9144902
Matt Nield <mattnield@gmail.com>
2018-11-07 00:07:13 +0000

    Upgrade to .NET 4.7.1 and upgrade all NuGet packages

--- PrizeDraw/MainWindow.xaml.cs
+++ PrizeDraw/MainWindow.xaml.cs
@@ -133,8 +133,9 @@ namespace PrizeDraw
                 }
                 case Key.Escape:
                 case Key.B:
-                {
+                {                  
                     _viewModel.Restart();
+                    _viewModel.Tiles.Remove(_viewModel.SelectedTile);
 
                     while (Canvas.Children.Count > 0)
                     {
@@ -163,6 +164,8 @@ namespace PrizeDraw
                     {
                         return;
                     }
+
+                        //var i = dlg.EventId;
 
                     await _eventValidator.InitAsync(dlg.EventId);
 
