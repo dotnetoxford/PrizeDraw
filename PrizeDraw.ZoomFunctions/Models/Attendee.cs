cb82d5db518adcc798e2f31e9c4f96516a50a328
Dan Clarke <dan@dracan.co.uk>
2020-04-11 08:52:46 +0100

    Add Azure Function for Zoom webhooks that populates Azure Table Storage
    Because the Zoom API doesn't allow us to get a list of users in a meeting on the free or Pro tier (you need business) - we're using Zoom's webhooks instead. This Azure Function will respond to Zoom webhook when someone enters and leaves the meeting, and add an entry in an Azure Table Storage table.

---
+++ PrizeDraw.ZoomFunctions/Models/Attendee.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace PrizeDraw.ZoomFunctions.Models
+{
+    public class Attendee : TableEntity
+    {
+        public Attendee(string meetingId)
+        {
+            PartitionKey = meetingId;
+            RowKey = Guid.NewGuid().ToString();
+        }
+
+        public string MeetingUuid { get; set; }
+
+        public string MeetingName { get; set; }
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public string UserUuid { get; set; }
+        public DateTime? JoinTime { get; set; }
+        public DateTime? LeaveTime { get; set; }
+    }
+}
